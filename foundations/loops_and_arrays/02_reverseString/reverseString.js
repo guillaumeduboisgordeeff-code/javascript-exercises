be8dce057f0f95f3c3b0758d626a6aa8b5a7f295
Guillaume Dubois <281606610+guillaumeduboisgordeeff-code@users.noreply.github.com>
2026-09-12 11:18:32 +0200

    Conceive a reverseString function based on split, reverse, join

--- foundations/loops_and_arrays/02_reverseString/reverseString.js
+++ foundations/loops_and_arrays/02_reverseString/reverseString.js
@@ -1,6 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    return string
+        .split("")
+        .reverse()
+        .join("")
 };
-
+console.log (reverseString("It is a good day to die"));
 // Do not edit below this line
 module.exports = reverseString;
