08f9ad68e3f65182b8c5f0c7421fc20293e0bfa0
Guillaume Dubois <281606610+guillaumeduboisgordeeff-code@users.noreply.github.com>
2026-09-12 14:18:41 +0200

    Conceive a function based on .filter method to removefromArray parameters

--- foundations/loops_and_arrays/03_removeFromArray/removeFromArray.js
+++ foundations/loops_and_arrays/03_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
-};
-
+const removeFromArray = function(arr) {
+    for(let i=1;i<arguments.length;i++){
+    arr=arr.filter(item=>item!==arguments[i]);
+    }
+    return arr;
+}
+console.log(removeFromArray([1,2,3,4,5],2,3));
 // Do not edit below this line
 module.exports = removeFromArray;
