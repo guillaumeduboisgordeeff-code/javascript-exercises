f25819a49905e431fc653cf2d4504fe2f3a04ba9
Guillaume Dubois <281606610+guillaumeduboisgordeeff-code@users.noreply.github.com>
2026-09-13 15:20:59 +0200

    Conceive a learYear function based on if and &&

--- foundations/loops_and_arrays/05_leapYears/leapYears.js
+++ foundations/loops_and_arrays/05_leapYears/leapYears.js
@@ -1,6 +1,14 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if (Number.isInteger(year/4) && !Number.isInteger(year/100)) {
+        return true;
+    }else if (Number.isInteger(year/4) && Number.isInteger(year/100) && Number.isInteger(year/400)) { 
+        return true; 
+    } else {
+        return false;
+    }
 };
+
+console.log(leapYears(1984))
 
 // Do not edit below this line
 module.exports = leapYears;
