1652136f4d362a82ebef200454fcd0c480c41d01
Guillaume Dubois <281606610+guillaumeduboisgordeeff-code@users.noreply.github.com>
2026-09-13 18:47:42 +0200

    Conceive formula to translate temperature from celsius to fahrenheit

--- foundations/loops_and_arrays/06_tempConversion/tempConversion.js
+++ foundations/loops_and_arrays/06_tempConversion/tempConversion.js
@@ -1,8 +1,24 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(value) {
+  const result = (value-32)*(5/9);
+  if (Number.isInteger(result)) {
+    return result
+  } else {
+    return Number(result.toFixed(1));
+  }
 };
 
-const convertToFahrenheit = function() {
+console.log(convertToCelsius(100));
+
+const convertToFahrenheit = function(input) {
+  const output = input*(9/5)+32;
+  if (Number.isInteger(output)) {
+    return output
+  } else {
+  return Number(output.toFixed(1));
+  }
 };
+
+console.log(convertToFahrenheit(0));
 
 // Do not edit below this line
 module.exports = {
