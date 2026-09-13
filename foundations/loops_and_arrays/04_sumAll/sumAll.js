42466067202513cee89c2dc4f4595a79e2257a51
Guillaume Dubois <281606610+guillaumeduboisgordeeff-code@users.noreply.github.com>
2026-09-13 15:01:47 +0200

    Create sumAll function based on loop push and reduce

--- foundations/loops_and_arrays/04_sumAll/sumAll.js
+++ foundations/loops_and_arrays/04_sumAll/sumAll.js
@@ -1,6 +1,15 @@
-const sumAll = function() {
+const sumAll = function(intA,intB) {
+ let arr =[]
+if(intA < 0 || intB <0 || !Number.isInteger(intA) || !Number.isInteger(intB)) {
+return "ERROR";
+} else { 
+    const minInt=Math.min(intA,intB);
+    const maxInt=Math.max(intA,intB);
+    for (let i=minInt;i<=maxInt;i++) {arr.push(i);}
+}
+return arr.reduce((sum,current)=>sum+current,0);
+}
 
-};
 
 // Do not edit below this line
 module.exports = sumAll;
